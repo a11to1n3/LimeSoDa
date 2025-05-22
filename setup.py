355134cb8cb86ab533eb7a01d54161febde07da2
Viacheslav Barkov <46378663+slavabarkov@users.noreply.github.com>
2025-05-22 16:18:50 +0200

    chore: bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="LimeSoDa",
-    version="0.1.4", 
+    version="0.1.5",
     author="Jonas Schmidinger",
     author_email="jonas.schmidinger@uni-osnabrueck.de",
     description="Precision Liming Soil Datasets (LimeSoDa) for Python",
